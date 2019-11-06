67a7c9b02230068a176c90aacb9cfbe4ab5f9f18
Nguyen Vuong <vlmnguyen98@gmail.com>
2019-11-06 20:51:48 +0700

    Fixed strict/unstrict rules

--- inference_engine/InferenceEngine.py
+++ inference_engine/InferenceEngine.py
@@ -79,7 +79,80 @@ class InferenceEngine:
         if isquery:
             varList.append(None)
         return {"name": rela, "var": varList}
-
+    
+    def getStrictRule(self, i, j):
+        F = self.facts
+        varMap = dict()
+        curobj = ord('A')
+        curnum = ord('a')
+        f1 = []
+        for v in F[i]["var"]:
+            if v is None:
+                f1.append('_')
+            elif type(v) is str:
+                f1.append(chr(curobj))
+                varMap[chr(curobj)] = v
+                curobj += 1
+            else:
+                f1.append(chr(curnum))
+                varMap[chr(curnum)] = v
+                curnum += 1
+        f2 = []
+        for v in F[j]["var"]:
+            if v not in F[i]["var"]:
+                if v is None:
+                    f2.append('_')
+                elif type(v) is str:
+                    f2.append(chr(curobj))
+                    varMap[chr(curobj)] = v
+                    curobj += 1
+                else:
+                    f2.append(chr(curnum))
+                    varMap[chr(curnum)] = v
+                    curnum += 1
+            else:
+                tmp = f1[F[i]["var"].index(v)]
+                f2.append(tmp)
+                varMap[tmp] = v
+        return varMap, "%s(%s)&%s(%s)" % (F[i]["name"], ';'.join(f1), F[j]["name"], ';'.join(f2))
+    
+    def getUnstrictRule(self, i, j):
+        F = self.facts
+        varMap = dict()
+        curobj = ord('A')
+        curnum = ord('a')
+        f1 = []
+        for v in F[i]["var"]:
+            if v is None:
+                f1.append('_')
+            elif type(v) is str:
+                f1.append(chr(curobj))
+                varMap[chr(curobj)] = v
+                curobj += 1
+            else:
+                f1.append(chr(curnum))
+                varMap[chr(curnum)] = v
+                curnum += 1
+        f2 = []
+        for v in F[j]["var"]:
+            if v is None:
+                f2.append('_')
+            elif type(v) is str:
+                if v not in F[i]["var"]:
+                    f2.append(chr(curobj))
+                    varMap[chr(curobj)] = v
+                    curobj += 1
+                else:
+                    tmp = f1[F[i]["var"].index(v)]
+                    f2.append(tmp)
+                    varMap[tmp] = v
+            else:
+                f2.append(chr(curnum))
+                varMap[chr(curnum)] = v
+                curnum += 1
+            
+        return varMap, "%s(%s)&%s(%s)" % (F[i]["name"], ';'.join(f1), F[j]["name"], ';'.join(f2))
+        
     def infer(self):
         F = self.facts
         F.extend(self.queries)
@@ -91,91 +164,52 @@ class InferenceEngine:
             rule = None
             # print("F::", F)
             for i in range(len(F)):
-                varMap = dict()
-                curobj1 = ord('A')
-                curnum1 = ord('a')
-                f1 = []
-                for v in F[i]["var"]:
-                    if v is None:
-                        f1.append('_')
-                    elif type(v) is str:
-                        f1.append(chr(curobj1))
-                        varMap[chr(curobj1)] = v
-                        curobj1 += 1
-                    else:
-                        f1.append(chr(curnum1))
-                        varMap[chr(curnum1)] = v
-                        curnum1 += 1
                 for j in range(len(F)):
-                    curobj2 = curobj1
-                    curnum2 = curnum1
-                    f2 = []
-                    for v in F[j]["var"]:
-                        if v not in F[i]["var"]:
-                            if v is None:
-                                f2.append('_')
-                            elif type(v) is str:
-                                f2.append(chr(curobj2))
-                                varMap[chr(curobj2)] = v
-                                curobj2 += 1
-                            else:
-                                f2.append(chr(curnum2))
-                                varMap[chr(curnum2)] = v
-                                curnum2 += 1
-                        else:
-                            tmp = f1[F[i]["var"].index(v)]
-                            f2.append(tmp)
-                            varMap[tmp] = v
-                    rule = "%s(%s)&%s(%s)" % (F[i]["name"], ';'.join(f1), F[j]["name"], ';'.join(f2))
-
-                    # print("ExploredRule::", rule)
-                    # print("ExploredFact::",str(F[i])+ "&" + str(F[j]))
-
                     if str(F[i])+ "&" + str(F[j]) in E:
                         continue
                     E.add(str(F[i])+ "&" + str(F[j]))
-                    if rule in R:
-                        match = True
-                        if None in F[i]["var"] + F[j]["var"] and F[i]["name"] == F[j]["name"]:
-                            nquery -= 1
-                        
-                        # print("Rule match::", rule)
-
-                        # Delete old facts:
-                        if i < j:
-                            i, j = j, i
-                        F.pop(i)
-                        F.pop(j)
-                        tmp = R[rule]
-
-                        # Get new fact
-                        if len(tmp) > 1:
-                            new_fact = {
-                                "name": tmp[1][: tmp[1].index("(")],
-                                "var": tmp[1][tmp[1].index("(")+1: -1].split(';')
-                            }
-                            for ind in range(len(new_fact["var"])):
-                                v = new_fact["var"][ind]
-                                if v in varMap:
-                                    new_fact["var"][ind] = varMap[v]
+                    for varMap, rule in (self.getStrictRule(i,j), self.getUnstrictRule(i,j)):
+                        # print("ExploredRule::", rule)
+                        # print("ExploredFact::",str(F[i])+ "&" + str(F[j]))
+                        if rule in R:
+                            match = True
+                            if None in F[i]["var"] + F[j]["var"] and F[i]["name"] == F[j]["name"]:
+                                nquery -= 1
+                            # print("Rule match::", rule)
+                            # Delete old facts:
+                            # if i < j:
+                            #     i, j = j, i
+                            # F.pop(i)
+                            # if len(F): F.pop(j)
+                            tmp = R[rule]
+                            # Create new fact
+                            if len(tmp) > 1:
+                                new_fact = {
+                                    "name": tmp[1][: tmp[1].index("(")],
+                                    "var": tmp[1][tmp[1].index("(")+1: -1].split(';')
+                                }
+                                for ind in range(len(new_fact["var"])):
+                                    v = new_fact["var"][ind]
+                                    if v in varMap:
+                                        new_fact["var"][ind] = varMap[v]
+                                    else:
+                                        func = self.opmap[v[:v.index('(')]]
+                                        new_fact["var"][ind] = func(
+                                            [varMap[item] for item in v[v.index('(')+1:-1].split(',')])
+                                F.append(new_fact)
+                            # Add to answer
+                            desc = tmp[0]
+                            for v in varMap:
+                                if v.isupper():
+                                    desc = desc.replace('$'+v, varMap[v])
                                 else:
-                                    func = self.opmap[v[:v.index('(')]]
-                                    new_fact["var"][ind] = func(
-                                        [varMap[item] for item in v[v.index('(')+1:-1].split(',')])
-                            F.append(new_fact)
+                                    desc = desc.replace('$'+v, str(varMap[v]))
 
-                        # Add to answer
-                        desc = tmp[0]
-                        for v in varMap:
-                            if v.isupper():
-                                desc = desc.replace('$'+v, varMap[v])
-                            else:
-                                desc = desc.replace('$'+v, str(varMap[v]))
-
-                        for opans in [self.opmap[o[1:o.index('(')]](int(t) for t in o[o.index('(')+1:-2].split(',')) for o in re.findall('\[.*\]', desc)]:
-                            desc = re.sub('\[.*\]', str(opans), desc)
-                        self.answer += desc
-                        break
+                            for opans in [self.opmap[o[1:o.index('(')]](int(t) for t in o[o.index('(')+1:-2].split(',')) for o in re.findall('\[.*\]', desc)]:
+                                desc = re.sub('\[.*\]', str(opans), desc)
+                            self.answer += desc
+                            break
+                    if match: break
                 if match: break
             if not match:
                 self.answer += "\n\nKhông thể tiếp tục quá trình suy luận!"
