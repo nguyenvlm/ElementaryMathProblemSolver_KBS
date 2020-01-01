5de3a87bec4aefd97f13423009490259eda29a5f
Luong Cong Tam <congtam.luong0406@gmail.com>
2020-01-01 23:21:54 +0700

    Open with  default browser when server started

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bot = require("./lib/spawn_bot")();
 const express = require("express");
 const http = require("http");
 const socketIO = require("socket.io");
+const open = require("open");
 const PORT = 8080;
 
 const app = express();
@@ -28,4 +29,9 @@ io.on("connection", socket => {
 server.listen(PORT, "0.0.0.0", error => {
   if (error) console.log(error);
   else console.log("Server is listening on Port", PORT);
+  open_in_browser();
 });
+
+async function open_in_browser() {
+  await open("http://localhost:8080");
+}
